766b98e8a6d4f552a325415a0c8ca12dc9accce6
Reka <reka.kovacs1993@gmail.com>
2023-04-24 13:20:29 +0100

    updated

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, redirect
 from app import app
 from models.books_list import *
 
@@ -11,27 +11,30 @@ def add_book():
     book_title = request.form["title"]
     book_author = request.form["author"]
     book_genre = request.form["genre"]
-    book_cover = request.form["image"]
+    book_cover = request.form["image"].replace(".jpeg", "")
 
     new_book = Book(book_title, book_author, book_genre, book_cover, False)
     add_new_book(new_book)
-    return render_template("index.html", books = books)
+    return redirect("/")
 
 @app.route("/remove/<int:index>", methods=["POST"])
 def remove_books(index):
     i = int(index)
     delete = request.form.get("delete")
-    print(delete)
     if delete:
         remove_book(books[i])
-    return render_template("index.html", books = books, book = books[i])
+    return render_template("index.html", books = books)
 
-# def remove_book(book_id):
-#     for book in books:
-#         if book.id == book_id:
-#             remove_book(book)
-#             break
-#     return redirect("/")
+
+# @app.route("/remove/<book>", methods=["POST"])
+# def remove_books(book):# book = 35book5
+#     # i = int(index)
+#     book = request.form.get("book")
+#     print(book)
+#     if book:
+#         remove_book(book)
+#     return render_template("index.html", books = books)
+
 
 
 @app.route("/book/<index>")
@@ -41,16 +44,3 @@ def book(index):
 
 
 
-# @app.route("/delete/<index>", methods=["POST"])
-# def remove_books(index):
-#     # book = request.form["book"]
-#     # remove_book(book)
-
-#     book_title = request.form["title"]
-#     book_author = request.form["author"]
-#     book_genre = request.form["genre"]
-#     new_book = Book(book_author, book_title, book_genre)
-#     remove_book(book1)
-#     return render_template("index.html", books = books)
-
-#     # return render_template("index.html", books = books, book = book)
--- models/books_list.py
+++ models/books_list.py
@@ -1,9 +1,9 @@
 from models.book import Book
 
 book1 = Book("Diary", "Chuck Palahniuk", "fiction", "0", True)
-book2 = Book("Solaris", "Stanislav Lem", "science-fiction", "1", False)
+book2 = Book("Solaris", "Stanislaw Lem", "science-fiction", "1", False)
 book3 = Book("Jubiabá", "Jorje Amado", "fiction", "2", True)
-book4 = Book("Ursula K LeGuin", "The Dispossessed", "science-fiction", "3", False)
+book4 = Book("Ursula K Le Guin", "The Dispossessed", "science-fiction", "3", False)
 
 
 books = [book1, book2, book3, book4]
